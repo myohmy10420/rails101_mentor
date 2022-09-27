9973140436b695985ac93ccf08cd33436592a09e
KurtHsu <kurt@psp-power.com.tw>
2022-09-27 19:15:55 +0800

    add aasm to post for build basic logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 class Post < ApplicationRecord
+  include AASM
+
   belongs_to :user
   belongs_to :group
 
   validates :content, presence: true
 
   scope :recent, -> { order("created_at DESC") }
+
+  aasm column: :status, enum: true do
+    state :pending, initial: true
+    state :publish, :unapprove, :cancel, :block
+  end
+
+  enum status: {
+    pending: 0,
+    publish: 1,
+    unapprove: 2,
+    cancel: 3, # by user
+    block: 4 # by group owner
+  }
 end
