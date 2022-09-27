069577d01b064884299248064abdc50f5efe69fa
KurtHsu <kurt@psp-power.com.tw>
2022-09-27 12:07:23 +0800

    implement group update and destroy posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i(new, create)
+  before_action :find_group
+  before_action :find_post_and_check_permission, only: %i(edit update destroy)
 
   def new
-    @group = Group.find(params[:group_id])
     @post = Post.new
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
@@ -19,9 +19,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to group_path(@group), alert: "Group deleted"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def find_post_and_check_permission
+    @post = Post.find(params[:id])
+
+    redirect_to root_path, alert: "You have no permission." if current_user != @post.user
   end
 end
