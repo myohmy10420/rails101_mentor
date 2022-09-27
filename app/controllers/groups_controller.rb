cdfecf1da79e82206b6ac718de8a4249ca04ac29
KurtHsu <kurt@psp-power.com.tw>
2022-09-27 09:35:51 +0800

    add group validation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,16 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
   def show
     @group = Group.find(params[:id])
   end
@@ -17,9 +27,12 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
 
-    redirect_to groups_path, notice: "Update Success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -27,13 +40,6 @@ class GroupsController < ApplicationController
     @group.destroy
 
     redirect_to groups_path, alert: "Group deleted"
-  end
-
-  def create
-    @group = Group.new(group_params)
-    @group.save
-
-    redirect_to groups_path
   end
 
   private
