80d43c74067b83ebc80f80eee663d407b7e83eee
KurtHsu <kurt@psp-power.com.tw>
2022-09-27 11:00:20 +0800

    only group owner can operate his own groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: %i(new create edit update destroy)
+  before_action :find_group_and_check_permission, only: %i(edit update destroy)
 
   def index
     @groups = Group.all
@@ -24,13 +25,9 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @group = Group.find(params[:id])
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -39,13 +36,18 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
 
     redirect_to groups_path, alert: "Group deleted"
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    redirect_to root_path, alert: "You have no permission." if current_user != @group.user
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
